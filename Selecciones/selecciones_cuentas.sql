f8b93ce2296a83e26ee86a75f8193c7cbf23f411
kevin <kevinmacash@gmail.com>
2025-11-03 10:14:15 -0500

    feat: completed selecciones exercises

---
+++ Selecciones/selecciones_cuentas.sql
@@ -0,0 +1,3 @@
+select numero_cuenta,saldo from cuentas;
+select * from cuentas  where fecha_creacion between current_date - interval '2 months' and current_date;
+select  numero_cuenta,saldo from cuentas where fecha_creacion between current_date - interval '2 months' and current_date;
